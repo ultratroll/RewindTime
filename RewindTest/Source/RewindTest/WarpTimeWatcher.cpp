43eca086d36b86035ad138679fcbe8f592445e4f
Terajoel (Jose De Jesus) <jdejesus@teravisiongames.com>
2018-02-08 17:01:54 -0500

    Adding Warp time watcher !.
    during pause it does a strange behaviour.

---
+++ RewindTest/Source/RewindTest/WarpTimeWatcher.cpp
@@ -0,0 +1,119 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "WarpTimeWatcher.h"
+#include "Components/ActorComponent.h"
+#include "Runtime/Engine/Classes/Kismet/GameplayStatics.h"
+#include "Engine/World.h"
+
+// Sets default values for this component's properties
+UWarpTimeWatcher::UWarpTimeWatcher()
+{
+	PrimaryComponentTick.bCanEverTick = true;
+
+	Records=  TArray<FWarpRecordData>();
+}
+
+
+void UWarpTimeWatcher::BeginPlay()
+{
+	Super::BeginPlay();
+	Records.Empty();
+	StartRecording();
+}
+
+
+void UWarpTimeWatcher::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+	if (bIsRewinding)
+	{
+		if ((CurrentRecordIndex - 1) >=0 )
+		{
+			if (!bIsInterpolatingRecords)
+			{
+				RecordDeltaTime = Records[CurrentRecordIndex].Time - Records[CurrentRecordIndex - 1].Time;
+				CurrentRecordDeltaTime = RecordDeltaTime;
+				bIsInterpolatingRecords = true;
+			}
+			CurrentRecordDeltaTime -= DeltaTime;
+			float Alpha = (1 - (CurrentRecordDeltaTime / RecordDeltaTime)); // This DeltaTime wont serve us in pause, will calculate our own with getrealtime
+
+			UE_LOG(LogTemp, Warning, TEXT(">> Before %d :: %f"), CurrentRecordIndex, Alpha);
+
+			InterpolateRecords(Records[CurrentRecordIndex], Records[CurrentRecordIndex - 1], Alpha);
+			if (Alpha >= 1)
+			{
+				bIsInterpolatingRecords= false;
+				CurrentRecordIndex--;
+			}
+		}
+		else {
+			// Is not possible to continue rewinding trough records 
+			bIsRewinding = false;
+		}
+
+		/* old way
+		ApplyRecordByIndex(CurrentRecordIndex);
+		CurrentRecordIndex--;
+		if (CurrentRecordIndex < 0) 
+			bIsRewinding = false;*/
+			
+	}
+}
+
+void UWarpTimeWatcher::SaveCurrentRecord()
+{
+	if (!bCanSaveRecords) 
+		return;
+	FWarpRecordData Record = FWarpRecordData();
+	Record.Position = this->GetOwner()->GetActorLocation();
+	Record.Rotation = this->GetOwner()->GetActorRotation();
+	Record.Time		= UGameplayStatics::GetRealTimeSeconds(GetWorld());
+	Records.Add(Record);
+}
+
+void UWarpTimeWatcher::StartRecording()
+{
+	bCanSaveRecords = true;
+	GetWorld()->GetTimerManager().SetTimer(RecordTimer, this, &UWarpTimeWatcher::SaveCurrentRecord, 0.05f, true); // 1/RecordSavingFrequency
+}
+
+void UWarpTimeWatcher::StopRecording()
+{
+	bCanSaveRecords = false;
+	GetWorld()->GetTimerManager().ClearTimer(RecordTimer);
+}
+
+void UWarpTimeWatcher::Rewind()
+{
+	StopRecording();
+	bIsRewinding = true;
+	CurrentRecordIndex = Records.Num()-1;
+}
+
+void UWarpTimeWatcher::Replay()
+{
+	StopRecording();
+}
+
+void UWarpTimeWatcher::ApplyRecordByIndex(int Index)
+{
+	this->GetOwner()->SetActorLocation(Records[Index].Position);
+	this->GetOwner()->SetActorRotation(Records[Index].Rotation);
+}
+
+void UWarpTimeWatcher::ApplyRecord(FWarpRecordData Record)
+{
+	this->GetOwner()->SetActorLocation(Record.Position);
+	this->GetOwner()->SetActorRotation(Record.Rotation);
+}
+
+void UWarpTimeWatcher::InterpolateRecords(FWarpRecordData StartRecord, FWarpRecordData EndRecord, float Alpha)
+{
+	FWarpRecordData Record = FWarpRecordData();
+	Record.Position = FMath::Lerp(StartRecord.Position, EndRecord.Position, Alpha);
+	Record.Rotation = FMath::Lerp(StartRecord.Rotation, EndRecord.Rotation, Alpha);
+	ApplyRecord(Record);
+}
+
+
---
+++ RewindTest/Source/RewindTest/WarpTimeWatcher.h
@@ -0,0 +1,137 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "WarpTimeWatcher.generated.h"
+
+class ActorComponent;
+
+/** Structure to record and hold historic data. */
+USTRUCT(BlueprintType)
+struct FWarpRecordData
+{ // Save time for each record
+	GENERATED_BODY()
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Record, meta = (AllowPrivateAccess = "true"))
+		FVector_NetQuantize Position;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Record, meta = (AllowPrivateAccess = "true"))
+		FRotator Rotation;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Record, meta = (AllowPrivateAccess = "true"))
+		FVector_NetQuantize Direction;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Record, meta = (AllowPrivateAccess = "true"))
+		float Time;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Record, meta = (AllowPrivateAccess = "true"))
+		FName AnimationName; // probably will use actions instead
+
+	void Destroy()
+	{
+		;
+	}
+
+	FWarpRecordData()
+	{
+		;
+	}
+};
+
+/** 
+	The time watcher component serves to record historic data and reproduce it backwards or forward at the needed speeds. Interpolating the record data. 
+*/
+UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
+class REWINDTEST_API UWarpTimeWatcher : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:	
+	UWarpTimeWatcher();
+
+protected:
+	virtual void BeginPlay() override;
+
+	/** Record data for all the character history. */
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Record, meta = (AllowPrivateAccess = "true"))
+	TArray<FWarpRecordData> Records;
+
+	/** This is component is allowed to save records?. */
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = Record, meta = (AllowPrivateAccess = "true"))
+	bool bCanSaveRecords;
+
+	/** This is component is rewinding records?. */
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = Record, meta = (AllowPrivateAccess = "true"))
+	bool bIsRewinding;
+
+	/** This is component is replaying records?. */
+	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = Record, meta = (AllowPrivateAccess = "true"))
+	bool bIsReplaying;
+
+	/** Times for second we are going to record data. */
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Record, meta = (AllowPrivateAccess = "true"))
+	int RecordSavingFrequency = 16;
+
+	/** Current record data index being reproduced (in rewind or replay). */
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Record, meta = (AllowPrivateAccess = "true"))
+	int CurrentRecordIndex = 0;
+
+	/** Handle to manage the recording timer. */
+	FTimerHandle RecordTimer;
+
+private:
+	/** If we are currently interpolating bettwen records. */
+	bool bIsInterpolatingRecords;
+
+	/** For internal use. The diference in time bettwen two records. */
+	float RecordDeltaTime;
+
+	/** For internal use. The current time bettwen two records being interpolated. */
+	float CurrentRecordDeltaTime;
+
+public:	
+	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
+
+	/** Lets save the current position, rotation, target, animation name and point of that animation. */
+	UFUNCTION(BlueprintCallable, BlueprintCallable, Category = Projectile)
+	void SaveCurrentRecord();
+
+	/** Lets start saving data. */
+	UFUNCTION(BlueprintCallable, BlueprintCallable, Category = Projectile)
+	void StartRecording();
+
+	/** Lets stop recording data. */
+	UFUNCTION(BlueprintCallable, BlueprintCallable, Category = Projectile)
+	void StopRecording();
+
+	/** Rewind. */
+	UFUNCTION(BlueprintCallable, BlueprintCallable, Category = Projectile)
+	void Rewind();
+
+	/** Replay. */
+	UFUNCTION(BlueprintCallable, BlueprintCallable, Category = Projectile)
+	void Replay();
+
+	/** 
+	*	Lets apply the record data in the given index. 
+	*	@param	Index	Index of record to be applied.
+	*/
+	UFUNCTION(BlueprintCallable, BlueprintCallable, Category = Projectile)
+	void ApplyRecordByIndex(int Index);
+
+	/**
+	*	Lets apply the record data.
+	*	@param	Record	Record to be applied.
+	*/
+	UFUNCTION(BlueprintCallable, BlueprintCallable, Category = Projectile)
+	void ApplyRecord(FWarpRecordData Record);
+
+	/**
+	*	Lets interpolate bettwen two records and apply the result.
+	*	@param	StartRecord	Record to be applied.
+	*	@param	EndRecord	Record to be applied.
+	*/
+	UFUNCTION(BlueprintCallable, BlueprintCallable, Category = Projectile)
+		void InterpolateRecords(FWarpRecordData StartRecord, FWarpRecordData EndRecord, float Alpha);
+};
